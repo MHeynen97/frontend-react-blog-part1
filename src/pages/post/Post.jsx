9e4cf926306a323285152c387b5677cb88f795af
M Heynen <merelheynen@gmail.com>
2026-01-07 14:57:47 +0100

    3.1. Post formulier maken

--- src/pages/post/Post.jsx
+++ src/pages/post/Post.jsx
@@ -1,8 +1,89 @@
-function Post(){
-    return(
-        <>
-            <h1>Post</h1>
-        </>
+import './Post.css';
+import {useState} from 'react';
+import calculateReadTime from '../../helpers/calculateReadTime.js';
+import {useNavigate} from 'react-router-dom';
+
+function Post() {
+    const [formState, setFormState] = useState({
+        title: '',
+        subtitle: '',
+        author: '',
+        content: '',
+    });
+
+    const navigate = useNavigate();
+
+    function handleChange(e) {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log({
+            ...formState,
+            shares: 0,
+            comments: 0,
+            created: new Date().toISOString(),
+            readTime: calculateReadTime(formState.content),
+        });
+
+        console.log('De blog is succesvol verzameld! 🌈');
+        navigate('/posts');
+    }
+
+    return (
+        <section className="new-post-section outer-content-container">
+            <div className="inner-content-container__text-restriction">
+                <form className="new-post-form" onSubmit={handleSubmit}>
+                    <h1>Post toevoegen</h1>
+                    <label htmlFor="post-title">Titel</label>
+                    <input
+                        type="text"
+                        id="post-title"
+                        name="title"
+                        required
+                        value={formState.title}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-subtitle">Subtitle</label>
+                    <input
+                        type="text"
+                        id="post-subtitle"
+                        name="subtitle"
+                        required
+                        value={formState.subtitle}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-author">Naam en achternaam</label>
+                    <input
+                        type="text"
+                        id="post-author"
+                        name="author"
+                        required
+                        value={formState.author}
+                        onChange={handleChange}
+                    />
+                    <label htmlFor="post-content">Blogpost</label>
+                    <textarea
+                        name="content"
+                        id="post-content"
+                        cols="30"
+                        rows="10"
+                        required
+                        minLength={300}
+                        maxLength={2000}
+                        value={formState.content}
+                        onChange={handleChange}></textarea>
+                    <button type="submit">
+                        Toevoegen
+                    </button>
+                </form>
+            </div>
+        </section>
     );
 }
 
