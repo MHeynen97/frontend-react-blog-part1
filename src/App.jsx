5051f6d2c370783e30a87faec04d0ea82db05efd
M Heynen <merelheynen@gmail.com>
2026-01-07 14:04:18 +0100

    1.2 navbar aanmaken en styling hiervoor toevoegen

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,41 @@
 import './App.css'
+import logoMedium from './assets/logo-medium.png';
 import Home from './pages/home/Home.jsx';
 import Post from './pages/post/Post.jsx';
 import Overview from './pages/overview/Overview.jsx';
 import Error from './pages/error/Error.jsx';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
     return (
     <>
-        <nav>
+        <nav className="navbar-outer-container">
+            <div className="inner-nav-container">
+                    <img src={logoMedium} alt="Logo that links to home page" className="logo-small" />
+            <ul className="nav">
+                <li>
+                    <NavLink
+                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                        to='/'>
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                        to='/post'>
+                        Post
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                        to='/overview'>
+                        Overview
+                    </NavLink>
+                </li>
+            </ul>
+            </div>
 
         </nav>
         <main>
